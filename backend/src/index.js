3fbaded4b2f0ca388d7fd04e36f9c3f44c9d2106
kimi <hykimigoyal@gmail.com>
2025-06-25 19:48:47 +0530

    issuesolved

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,11 +33,11 @@ res.send("hii")
 
 
 
-if (process.env.NODE_ENV==="production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
