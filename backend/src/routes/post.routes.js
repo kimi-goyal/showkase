12effed2f5761c69096f1f02074eb9ace33cc0c9
kimi <hykimigoyal@gmail.com>
2025-06-25 19:55:52 +0530

    issuesolving

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,19 +1,34 @@
 import express from 'express';
-import {createPost, getPostById, getAllPosts, deletePosts, getPostsByArtistController, addComment, toggleLike, deleteComment} from "../controllers/post.controller.js"
-import { protectRoute } from '../middlewares/auth.middleware.js';
+import {
+  createPost,
+  getPostById,
+  getAllPosts,
+  deletePosts,
+  getPostsByArtistController,
+  addComment,
+  toggleLike,
+  deleteComment
+} from "../controllers/post.controller.js";
+
 import { getArtistDashboardStats } from '../controllers/artist.contoller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
+
 const route = express.Router();
 
-route.post("/create",protectRoute, createPost);
+route.post("/create", protectRoute, createPost);
 route.get("/posts", getAllPosts);
-route.get('/my-posts/:id', getPostsByArtistController);
+
+// 🟢 Group these under /artist/
+route.get('/artist/my-posts/:id', getPostsByArtistController);
+route.get('/artist/dashboard/:id', getArtistDashboardStats);
+
+// 🟢 Move this to the bottom so it doesn’t clash with others
+route.get("/:id", getPostById);
+
+// 🟢 Comments & Likes
 route.post("/:id/like", protectRoute, toggleLike);
 route.post("/:id/comment", protectRoute, addComment);
 route.delete('/:id/comment/:commentId', protectRoute, deleteComment);
 route.delete("/deletePost/:id", protectRoute, deletePosts);
-route.get('/dashboard/:id', getArtistDashboardStats);
-
-route.get("/:id", getPostById);
-
 
 export default route;
